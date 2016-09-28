e03256bea518e454d8bd4bb0067e685c12510005
andrewhills <andrewhills@users.noreply.github.com>
2016-09-28 16:19:29 +1000

    aa

---
+++ init.el
@@ -0,0 +1,607 @@
+
+;;===
+(add-to-list 'exec-path "/usr/local/bin")
+
+;;===site-lisp location
+(let ((default-directory "~/.emacs.d/site-lisp"))
+  (when (file-directory-p default-directory)
+    (add-to-list 'load-path default-directory)
+    (normal-top-level-add-subdirs-to-load-path)))
+
+;;===environments
+(setenv "PKG_CONFIG_PATH" "/usr/local/lib/pkgconfig:$PKG_CONFIG_PATH")
+(setenv "PYTHONPATH" "/usr/local/share/python:$PYTHONPATH")
+(setenv "CPATH" "$CPATH:/usr/local/include")
+(setenv "LIBRARY_PATH" "/usr/local/lib:/usr/local/share/python:$LIBRARY_PATH")
+(setenv "NODE_PATH" "/usr/local/lib/node_modules:$NODE_PATH")
+(setenv "NODE_NO_READLINE" "1")
+
+;;===disable bell
+(setq ring-bell-function nil)
+
+;;===no query kill processes, buffers
+(defadvice save-buffers-kill-emacs (around no-query-kill-emacs activate)
+  (mapc
+   (lambda (p) (set-process-query-on-exit-flag p nil))
+   (process-list))
+  ad-do-it)
+
+(setq kill-buffer-query-functions nil)
+
+;;===my-keys
+(defun my-local-set-key (k &rest l)
+  (local-set-key k
+    `(lambda ()
+       (interactive)
+       (mapc 'call-interactively (quote (,@l))))))
+
+(defun my-global-set-key (k &rest l)
+  (global-set-key k
+    `(lambda () (interactive) (mapc 'call-interactively (quote (,@l))))))
+
+;;===
+(defun my-kill-buffers (&rest n)
+  (mapc
+   '(lambda (x)
+      (let ((b (get-buffer x)))
+        (when b (kill-buffer b)))) n)
+  (delete-other-windows))
+
+;;===elisp
+(defun my-emacs-lisp-hook ()
+  (my-local-set-key (kbd "<f7>") 'delete-other-windows)
+  (mapc
+   '(lambda (sym) (put sym 'lisp-indent-function 'defun))
+   '(add-hook my-local-set-key my-global-set-key
+      global-set-key local-set-key)))
+
+(add-hook 'emacs-lisp-mode-hook 'my-emacs-lisp-hook)
+
+;;===fix some terminals keys
+(unless (display-graphic-p)
+  ;; (when (equal "xterm" (tty-type))
+  ;;   (define-key input-decode-map "\e[1;2A" [S-up]))
+  ;; (global-set-key "\M-[1~" 'beginning-of-line)
+  (global-set-key [select] 'end-of-line))
+
+;;===delete trailing space on save
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
+
+;;===windows undo key
+(global-set-key "\C-z"
+  'undo-only)
+
+;;===ido
+(defun op-i:dired ()
+  (interactive)
+  (let ((dir (car (find-file-read-args "Dired: " nil))))
+    (if (file-directory-p dir)
+        (dired dir "-lhA")
+      (find-file dir))))
+
+(global-set-key (kbd "C-x C-d") 'op-i:dired)
+
+;;===mouse2 select copy
+(defadvice mouse-save-then-kill (around mouse2-copy-region activate)
+  (unless mark-active
+    (move-beginning-of-line nil)
+    (skip-chars-forward " \t")
+    (set-mark-command nil)
+    (move-end-of-line nil)
+    (skip-chars-backward " \t") )
+  ;; (my-region-line)
+  (if (region-active-p)
+      (copy-region-as-kill (region-beginning) (region-end)))
+  ad-do-it)
+
+;;===
+(defun my-region-line ()
+  (interactive)
+  (unless (region-active-p)
+    (move-beginning-of-line nil)
+    (skip-chars-forward " \t")
+    (set-mark-command nil)
+    (move-end-of-line nil)
+    (skip-chars-backward " \t")
+    ;; (skip-chars-backward " \t")
+    ;; (set-mark-command nil)
+    ;; (setq deactivate-mark nil)
+    ))
+
+(defun my-region-expand ()
+  (interactive)
+  (when (region-active-p)
+    (let ((a (save-excursion
+               (goto-char (region-beginning))
+               (line-beginning-position 1)))
+          (b (save-excursion
+               (goto-char (region-end)) (line-end-position 1))))
+      (goto-char a)
+      (set-mark-command nil)
+      (goto-char b)
+      (move-end-of-line nil))))
+
+(defun my-region-indents ()
+  (interactive)
+  (unless (region-active-p)
+    (save-match-data
+      (let ((a (line-beginning-position 1)))
+        (goto-char a)
+        (let* ((ptn "^\\([ \t]+\\)[^\r\n]*\\([\r\n]+\\1[ \t]+[^\r\n]*\\)*")
+               (r (re-search-forward ptn (point-max) t))
+               (m (and r (match-end 0)))
+               (b (or m (line-end-position 1))))
+          (goto-char a)
+          (set-mark-command nil)
+          (goto-char b)
+          (move-end-of-line nil))))))
+
+;;===shift indent
+(defun shift-indent (amount)
+  (interactive "nShift indent by: ")
+  (save-excursion
+    (if (region-active-p)
+        (my-region-expand)
+      (my-region-indents))
+    (indent-rigidly (region-beginning) (region-end) amount)
+    (setq deactivate-mark nil)))
+
+;;===cut/copy
+(my-global-set-key "\C-w"
+  'my-region-line 'kill-region)
+
+(my-global-set-key "\M-w"
+  'my-region-line 'copy-region-as-kill)
+
+;;===comment region
+(my-global-set-key [f3]
+  'my-region-expand 'my-region-line 'uncomment-region)
+
+(my-global-set-key [f4]
+  'my-region-expand 'my-region-line 'comment-region)
+
+;;===comint history
+(defun my-comint-read-history ()
+  (let* ((buf (buffer-name (current-buffer)))
+         (fn (replace-regexp-in-string "\*+" "" buf))
+         (hist (concat user-emacs-directory "/hist"))
+         (comint-input-ring-file-name (concat hist "/" fn)))
+    (if (file-exists-p comint-input-ring-file-name)
+        (comint-read-input-ring)
+      (while (not (ring-empty-p comint-input-ring))
+        (ring-remove comint-input-ring nil)))))
+
+(defun my-comint-write-history ()
+  (when (derived-mode-p 'comint-mode)
+    (let* ((buf (buffer-name (current-buffer)))
+           (fn (replace-regexp-in-string "\*+" "" buf))
+           (hist (concat user-emacs-directory "/hist"))
+           (comint-input-ring-file-name (concat hist "/" fn)))
+      (unless (file-exists-p hist) (make-directory hist))
+      (comint-write-input-ring))))
+
+(defun my-comint-write-buffer-history (b)
+  (with-current-buffer b
+    (funcall 'my-comint-write-history)))
+
+(defun my-comint-write-histories ()
+  (mapc 'my-comint-write-buffer-history (buffer-list)))
+
+(add-hook 'comint-mode-hook 'my-comint-read-history)
+(add-hook 'kill-buffer-hook 'my-comint-write-history)
+(add-hook 'kill-emacs-hook 'my-comint-write-histories)
+
+;;===shell
+(defun my-shell-run (cmd bufnm)
+  (interactive)
+  (let* ((c (current-buffer))
+         (fn (buffer-file-name c))
+         (default-directory (file-name-directory fn)))
+    (unless (get-buffer-process bufnm)
+      (async-shell-command cmd bufnm)
+      (set-process-sentinel
+       (get-buffer-process bufnm)
+       `(lambda (process event)
+          (let ((cc (current-buffer)))
+            (when (eq cc (get-buffer ,bufnm))
+              (switch-to-buffer-other-window ,c)))
+          (message (substring event 0 (- (length event) 1))) ))
+      (switch-to-buffer-other-window bufnm))))
+
+;;===scheme
+(defun my-scheme-start ()
+  (interactive)
+  (let ((c (current-buffer)))
+    (unless (get-buffer "*scheme*")
+      (call-interactively 'run-scheme))
+    (switch-to-buffer-other-window "*scheme*")
+    (switch-to-buffer-other-window c)))
+
+(defun my-scheme-file ()
+  (interactive)
+  (scheme-load-file (buffer-file-name (current-buffer))))
+
+(defun my-scheme-send ()
+  (interactive)
+  (if (region-active-p)
+      (scheme-send-region (region-beginning) (region-end))
+    (scheme-send-last-sexp)))
+
+(defun my-scheme-kill ()
+  (interactive)
+  (my-kill-buffers "*scheme*"))
+
+(defun my-scheme-hook ()
+  (put 'mymatch 'scheme-indent-function 1)
+  (put 'match 'scheme-indent-function 1)
+  (put 'qmatch 'scheme-indent-function 1)
+  (my-local-set-key (kbd "<f5>")
+    'save-buffer 'my-scheme-start 'my-scheme-file)
+  (my-local-set-key (kbd "<f6>")
+    'save-buffer 'my-scheme-start 'my-scheme-send)
+  (my-local-set-key (kbd "<f7>")
+    'my-scheme-kill))
+
+(add-hook 'scheme-mode-hook 'my-scheme-hook)
+(add-to-list 'auto-mode-alist '("\\.rkt$" . scheme-mode))
+
+;;===latex
+(defun my-latex-file ()
+  (interactive)
+  (let* ((fn (buffer-file-name (current-buffer)))
+         (out-dir (concat (file-name-directory fn) "bin")))
+    (unless (file-exists-p out-dir)
+      (make-directory out-dir))
+    (let ((cmd (concat "pdflatex -output-directory="
+                       out-dir " -halt-on-error " fn))
+          (default-directory (concat (file-name-directory fn))))
+      (if (= 0 (shell-command cmd "*tex-shell*"))
+          (progn (message "success") (delete-windows-on "*tex-shell*"))
+        (let ((c (current-buffer)))
+          (switch-to-buffer-other-window "*tex-shell*")
+          (end-of-buffer)
+          (switch-to-buffer-other-window c)
+          (message "failed") )))))
+
+(defun my-latex-kill ()
+  (interactive)
+  (my-kill-buffers "*tex-shell*"))
+
+(defun my-latex-hook ()
+  (my-local-set-key (kbd "<f5>")
+    'save-buffer 'my-latex-file)
+  (my-local-set-key (kbd "<f7>")
+    'my-latex-kill))
+
+(add-hook 'latex-mode-hook 'my-latex-hook)
+
+;;===prolog
+(defun my-prolog-send-solve ()
+  (interactive)
+  (process-send-string "*prolog*" "solve(L), print(L), nl, false.\n"))
+
+(defun my-prolog-create-rc-file ()
+  (interactive)
+  (let (fn (if (eq 'windows-nt system-type) "~/pl.ini" "~/.plrc"))
+    (unless (file-exists-p fn)
+      (with-temp-file fn
+        (princ ":- set_prolog_flag(" (current-buffer))
+        (princ "toplevel_print_options," (current-buffer))
+        (princ "[quoted(true),portray(true)])." (current-buffer)) ))))
+
+(defun my-prolog-kill ()
+  (interactive)
+  (my-kill-buffers "*prolog*"))
+
+(defun my-prolog-hook ()
+  (my-local-set-key (kbd "<f5>")
+    'save-buffer 'run-prolog 'other-window
+    'prolog-consult-file 'other-window)
+  (my-local-set-key (kbd "S-<f5>")
+    'save-buffer 'run-prolog 'other-window
+    'prolog-consult-file 'other-window 'my-prolog-send-solve)
+  (my-local-set-key (kbd "<f6>")
+    'save-buffer'run-prolog 'other-window
+    'my-region-line 'prolog-consult-region 'other-window)
+  (my-local-set-key (kbd "<f7>")
+    'my-prolog-kill)
+  (my-local-set-key (kbd "<f8>")
+    'my-prolog-send-solve)
+  (my-local-set-key (kbd "<f9>")
+    'my-prolog-create-rc-file))
+
+(add-hook 'prolog-mode-hook 'my-prolog-hook)
+(add-to-list 'auto-mode-alist '("\\.pl$" . prolog-mode))
+
+;;===python
+(defun my-python-start ()
+  (interactive)
+  (unless (get-buffer "*Python*")
+    (call-interactively 'run-python)
+    (other-window 1)))
+
+(defun my-python-kill ()
+  (interactive)
+  (my-kill-buffers "*Python*"))
+
+(defun my-python-send-buffer ()
+  (interactive)
+  (python-shell-send-buffer t))
+
+(defun my-python-send-line ()
+  (interactive)
+  (my-region-line)
+  (python-shell-send-region (region-beginning) (region-end) t))
+
+(defun my-python-hook ()
+  (my-local-set-key (kbd "<f5>")
+    'save-buffer 'my-python-start 'my-python-send-buffer)
+  (my-local-set-key (kbd "<f6>")
+    'save-buffer 'my-python-start 'my-python-send-line)
+  (my-local-set-key (kbd "<f7>") 'my-python-kill))
+
+(add-hook 'python-mode-hook 'my-python-hook)
+
+;;===tcl
+(defun my-tcl-file ()
+  (interactive)
+  (tcl-load-file (buffer-file-name (current-buffer))))
+
+(defun my-tcl-send ()
+  (interactive)
+  (my-region-line)
+  (tcl-eval-region (region-beginning) (region-end))
+  (setq mark-active nil))
+
+(defun my-tcl-start ()
+  (interactive)
+  (let ((c (current-buffer)))
+    (if (get-buffer "*inferior-tcl*")
+        (switch-to-buffer-other-window "*inferior-tcl*")
+      (call-interactively 'inferior-tcl))
+    (switch-to-buffer-other-window c)))
+
+(defun my-tcl-kill ()
+  (interactive)
+  (my-kill-buffers "*inferior-tcl*"))
+
+(defun my-tcl-hook ()
+  (my-local-set-key (kbd "<f5>")
+    'save-buffer 'my-tcl-start 'my-tcl-file)
+  (my-local-set-key (kbd "<f6>")
+    'save-buffer 'my-tcl-start 'my-tcl-send)
+  (my-local-set-key (kbd "<f7>")
+    'my-tcl-kill))
+
+(add-hook 'tcl-mode-hook 'my-tcl-hook)
+
+;;===cpp
+(defun my-cpp-compile ()
+  (interactive)
+  (let ((c (current-buffer)))
+    (call-interactively 'compile)
+    (switch-to-buffer-other-window "*compilation*")
+    (switch-to-buffer-other-window c)))
+
+(defun my-cpp-run ()
+  (interactive)
+  (my-shell-run "make test" "*c-run*"))
+
+(defun my-cpp-clean ()
+  (interactive)
+  (shell-command "make clean"))
+
+(defun my-cpp-kill ()
+  (interactive)
+  (my-kill-buffers "*compilation*" "*c-run*"))
+
+(defun my-cpp-hook ()
+  (set (make-local-variable 'comment-start) "//")
+  (set (make-local-variable 'comment-end) "")
+  (my-local-set-key (kbd "<f5>") 'save-buffer 'my-cpp-compile)
+  (my-local-set-key (kbd "<f6>") 'save-buffer 'my-cpp-run)
+  (my-local-set-key (kbd "<f7>") 'my-cpp-kill)
+  (my-local-set-key (kbd "<f8>") 'my-cpp-clean))
+
+(add-to-list 'auto-mode-alist '("\\.[ch]p?p?\\'" . c++-mode))
+(add-hook 'c++-mode-hook 'my-cpp-hook)
+(add-to-list 'auto-mode-alist '("\\.rl$" . c++-mode))
+
+;;===nodejs
+(defun my-nodejs-start ()
+  (interactive)
+  (let ((c (current-buffer)))
+    (if (get-buffer "*nodejs-repl*")
+        (pop-to-buffer "*nodejs-repl*")
+      (pop-to-buffer
+       (make-comint "nodejs-repl" "node" nil "--interactive")))
+    (switch-to-buffer-other-window c) ))
+
+(defun my-nodejs-send-file ()
+  (interactive)
+  (let* ((fn (buffer-file-name))
+         (str (format "eval(fs.readFileSync('%s')+'');\n" fn)))
+    (comint-send-string "*nodejs-repl*" str) ))
+
+(defun my-nodejs-send-region (start end)
+  (interactive "r")
+  (let ((str (concat (buffer-substring start end) "\n")))
+    (save-excursion
+      (my-region-line)
+      (comint-send-string "*nodejs-repl*" str) )))
+
+(defun my-nodejs-kill ()
+  (interactive)
+  (my-kill-buffers "*nodejs-repl*"))
+
+(defun my-nodejs-hook ()
+  (my-local-set-key (kbd "<f5>")
+    'save-buffer 'my-nodejs-start 'my-nodejs-send-file)
+  (my-local-set-key (kbd "<f6>")
+    'save-buffer 'my-nodejs-start 'my-nodejs-send-region)
+  (my-local-set-key (kbd "<f7>")
+    'my-nodejs-kill))
+
+(add-hook 'js-mode-hook 'my-nodejs-hook)
+
+;;===common lisp
+(defun my-lisp-start ()
+  (interactive)
+  (unless (get-buffer "*inferior-lisp*")
+    (let ((c (current-buffer)))
+      (call-interactively 'run-lisp)
+      (switch-to-buffer-other-window "*inferior-lisp*")
+      (switch-to-buffer-other-window c))))
+
+(defun my-lisp-file ()
+  (interactive)
+  (lisp-load-file (buffer-file-name (current-buffer)))
+  (other-window 1))
+
+(defun my-lisp-send ()
+  (interactive)
+  (if (region-active-p)
+      (lisp-eval-region (region-beginning) (region-end))
+    (lisp-eval-last-sexp)))
+
+(defun my-lisp-kill ()
+  (interactive)
+  (my-kill-buffers "*inferior-lisp*"))
+
+(defun my-lisp-hook ()
+  (my-local-set-key (kbd "<f5>")
+    'save-buffer 'my-lisp-start 'my-lisp-file)
+  (my-local-set-key (kbd "<f6>")
+    'save-buffer 'my-lisp-start 'my-lisp-send)
+  (my-local-set-key (kbd "<f7>")
+    'my-lisp-kill) )
+
+(add-hook 'lisp-mode-hook 'my-lisp-hook)
+
+;;===sql
+(defun my-sql-connect ()
+  (interactive)
+  (setq
+   sql-product
+   (intern
+    (read-string
+     (format "product (%s):" sql-product)
+     nil nil (symbol-name sql-product)))
+   sql-server
+   (read-string
+    (format"server (%s):" sql-server)
+    nil nil sql-server)
+   sql-port
+   (string-to-number
+    (read-string
+     (format "port (%i):" sql-port)
+     nil nil (number-to-string sql-port)))
+   sql-user
+   (read-string
+    (format "user (%s):" sql-user)
+    nil nil sql-user)
+   sql-password
+   (read-string
+    (format "pw (%s):" sql-password)
+    nil nil sql-password)
+   sql-database
+   (read-string
+    (format "db (%s):" sql-database)
+    nil nil sql-database))
+  (execute-kbd-macro
+   (vconcat [?\M-x] "sql-product-interactive")))
+
+(defun my-sql-kill ()
+  (interactive)
+  (my-kill-buffers "*SQL*"))
+
+(defun my-sql-hook ()
+  (my-local-set-key (kbd "<f5>")
+    'save-buffer 'sql-send-buffer)
+  (my-local-set-key (kbd "<f6>")
+    'save-buffer 'my-region-expand 'my-region-line 'sql-send-region)
+  (my-local-set-key (kbd "<f7>")
+    'my-sql-kill)
+  (my-local-set-key (kbd "<f9>")
+    'my-sql-connect 'other-window))
+
+(add-hook 'sql-mode-hook 'my-sql-hook)
+
+;;===extra
+(require 'my-extra nil t)
+(require 'simple-tabbar-mode nil t)
+
+;;===
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(c-basic-offset 2)
+ '(comint-scroll-to-bottom-on-input t)
+ '(compilation-read-command nil)
+ '(compile-command "make")
+ '(cursor-type (cons (quote bar) 1))
+ '(delete-selection-mode t)
+ '(desktop-save-mode t)
+ '(electric-pair-mode t)
+ '(global-auto-revert-mode t)
+ '(hi-lock-auto-select-face t)
+ '(icomplete-mode t)
+ '(icomplete-prospects-height 1)
+ '(ido-auto-merge-work-directories-length -1)
+ '(ido-create-new-buffer (quote always))
+ '(ido-default-buffer-method (quote selected-window))
+ '(ido-enable-flex-matching t)
+ '(ido-everywhere t)
+ '(ido-mode (quote both) nil (ido))
+ '(ido-save-directory-list-file "~/.emacs.d/.ido.last")
+ '(ido-use-filename-at-point (quote guess))
+ '(indent-tabs-mode nil)
+ '(inferior-lisp-program "sbcl")
+ '(js-indent-level 2)
+ '(linum-delay t)
+ '(linum-eager nil)
+ '(make-backup-files nil)
+ '(menu-bar-mode nil)
+ '(mouse-drag-copy-region nil)
+ '(mouse-wheel-follow-mouse t)
+ '(mouse-wheel-scroll-amount (list 1))
+ '(prolog-indent-width 2)
+ '(prolog-system (quote swi))
+ '(python-indent-guess-indent-offset nil)
+ '(python-indent-offset 2)
+ '(python-shell-interpreter "python")
+ '(scheme-program-name "racket")
+ '(show-paren-delay 0.0)
+ '(show-paren-mode t)
+ '(show-paren-style (quote parenthesis))
+ '(sql-database "test")
+ '(sql-password "pass")
+ '(sql-port 3306)
+ '(sql-product (quote mysql))
+ '(sql-server "alarmpi")
+ '(sql-user "root")
+ '(tcl-application "tclsh")
+ '(tcl-continued-indent-level 2)
+ '(tcl-indent-level 2)
+ '(tool-bar-mode nil)
+ '(x-select-enable-clipboard t)
+ '(x-select-enable-clipboard-manager t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(hi-black-b ((t (:background "DarkOrchid1"))))
+ '(hi-black-hb ((t (:background "MediumPurple1"))))
+ '(hi-blue ((t (:background "SteelBlue1"))))
+ '(hi-blue-b ((t (:background "CadetBlue2"))))
+ '(hi-green ((t (:background "IndianRed1"))))
+ '(hi-green-b ((t (:background "gray64"))))
+ '(hi-pink ((t (:background "SeaGreen2"))))
+ '(hi-red-b ((t (:background "goldenrod2"))))
+ '(hi-yellow ((t (:background "gold"))))
+ '(region ((t (:background "CornflowerBlue"))))
+ '(show-paren-match ((t (:background "blue" :bold t))))
+ '(show-paren-mismatch ((t (:background "purple" :bold t)))))
---
+++ my-extra.el
@@ -0,0 +1,288 @@
+
+;;===common lisp with slime
+(defun my-slime-switch-buffer ()
+  (interactive)
+  (let ((c (current-buffer)))
+    (switch-to-buffer-other-window "*inferior-lisp*")
+    (switch-to-buffer-other-window c)))
+
+(defun my-slime-kill-buffer ()
+  (interactive)
+  (when (slime-connected-p) (slime-quit-lisp))
+  (my-kill-buffers "*slime-events*" "*inferior-lisp*"))
+
+(defun my-slime-after-load ()
+  (setq slime-auto-connect 'always)
+  (makunbound 'inferior-lisp-program)
+  (defcustom inferior-lisp-program "lisp" "" :type 'string)
+  (slime-setup '(slime-fancy)) )
+
+(defun my-slime-hook ()
+  (my-local-set-key (kbd "<f5>")
+    'save-buffer 'slime-eval-buffer 'my-slime-switch-buffer)
+  (my-local-set-key (kbd "<f6>")
+    'save-buffer 'slime-eval-defun 'my-slime-switch-buffer)
+  (my-local-set-key (kbd "<f7>") 'my-slime-kill-buffer) )
+
+(defadvice my-slime-lisp-hook (around my-lisp-hook activate)
+   ad-do-it
+  (condition-case nil (slime-mode) (file-error nil)))
+
+(add-hook 'slime-mode-hook 'my-slime-hook)
+(autoload 'slime-mode "slime" "common lisp..." t)
+(eval-after-load "slime" '(my-slime-after-load))
+
+;;===lua
+(defun my-lua-kill ()
+  (interactive)
+  (my-kill-buffers "*lua*"))
+
+(defun my-lua-hook ()
+  (my-local-set-key (kbd "<f5>")
+    'save-buffer 'lua-send-buffer)
+  (my-local-set-key (kbd "<f6>")
+    'save-buffer 'my-region-line 'lua-send-region)
+  (my-local-set-key (kbd "<f7>")
+    'my-lua-kill))
+
+(add-hook 'lua-mode-hook 'my-lua-hook)
+(autoload 'lua-mode "lua-mode" "Lua editing mode." t)
+(add-to-list 'auto-mode-alist '("\\.lua$" . lua-mode))
+(add-to-list 'interpreter-mode-alist '("lua" . lua-mode))
+
+;;===java
+(defun my-java-compile ()
+  (interactive)
+  (compile "ant -emacs compile -find" t))
+
+(defun my-java-jar ()
+  (interactive)
+  (compile "ant -emacs jar -find" t))
+
+(defun my-java-run ()
+  (interactive)
+  (compile "ant -emacs run -find" t))
+
+(defun my-java-test ()
+  (interactive)
+  (compile "ant -emacs test -find" t))
+
+(defun my-java-clean ()
+  (interactive)
+  (compile "ant -emacs clean -find" t))
+
+(defun my-java-kill ()
+  (interactive)
+  (my-kill-buffers "*compilation*"))
+
+(defun my-java-hook ()
+  (my-local-set-key (kbd "<f5>")
+    'save-buffer 'my-java-compile)
+  (my-local-set-key (kbd "<f6>")
+    'my-java-run)
+  (my-local-set-key (kbd "<f7>")
+    'my-java-kill)
+  (my-local-set-key (kbd "<f8>")
+    'my-java-clean))
+
+(add-hook 'java-mode-hook 'my-java-hook)
+
+;;===scala
+(defun my-scala-start ()
+  (interactive)
+  (unless (get-buffer-process "*sbt*")
+    (let ((c (current-buffer)))
+      (async-shell-command "sbt" "*sbt*")
+      (compilation-shell-minor-mode t)
+      (switch-to-buffer-other-window "*sbt*")
+      (switch-to-buffer-other-window c) )))
+
+(defun my-scala-run ()
+  (interactive)
+  (comint-send-string "*sbt*" "run\n"))
+
+(defun my-scala-compile ()
+  (interactive)
+  (comint-send-string "*sbt*" "compile\n"))
+
+(defun my-scala-kill ()
+  (interactive)
+  (my-kill-buffers "*sbt*"))
+
+(defun my-scala-hook ()
+  (set (make-local-variable 'compilation-auto-jump-to-first-error) t)
+  (set (make-local-variable 'comint-scroll-to-bottom-on-output) t)
+  (set (make-local-variable 'comint-output-filter-functions)
+       '(ansi-color-process-output comint-postoutput-scroll-to-bottom))
+  (my-local-set-key (kbd "<f5>")
+    'save-buffer 'my-scala-start 'my-scala-compile)
+  (my-local-set-key (kbd "<f6>")
+    'save-buffer 'my-scala-start 'my-scala-run)
+  (my-local-set-key (kbd "<f7>")
+    'my-scala-kill) )
+
+(add-hook 'scala-mode-hook 'my-scala-hook)
+(autoload 'scala-mode "scala-mode2" "scala..." t)
+(add-to-list 'auto-mode-alist '("\\.scala$" . scala-mode))
+
+;;===haskell
+(defun my-haskell-kill ()
+  (interactive)
+  (my-kill-buffers "*haskell*"))
+
+(defun my-haskell-hook ()
+  (turn-on-haskell-indentation)
+  (local-unset-key  (kbd "C-x C-d"))
+  (my-local-set-key (kbd "<f5>")
+    'save-buffer 'inferior-haskell-load-and-run
+    'recenter-top-bottom 'other-window)
+  (my-local-set-key (kbd "<f7>")
+    'my-haskell-kill))
+
+(add-hook 'haskell-mode-hook 'my-haskell-hook)
+(autoload 'haskell-mode "inf-haskell" "haskell..." t)
+(add-to-list 'auto-mode-alist '("\\.hs$" . haskell-mode))
+(eval-after-load "inf-haskell" '(require 'haskell-indentation nil t))
+(eval-after-load "inf-haskell" '(require 'haskell-font-lock nil t))
+
+;;===erlang
+(let ((p (file-expand-wildcards "/usr/lib/erlang/lib/tools*")))
+  (when p (add-to-list 'load-path (concat (car p) "/emacs"))))
+
+(defun my-erlang-compile ()
+  (interactive)
+  (let ((c (current-buffer)))
+    (call-interactively 'erlang-compile)
+    (unless (eq c (current-buffer))
+      (other-window))))
+
+(defun my-erlang-run ()
+  (interactive)
+  (let* ((fn (buffer-file-name (current-buffer)))
+         (n (file-name-sans-extension (file-name-nondirectory fn))))
+    ;; (inferior-erlang-send-command (concat n ":main()."))
+    (comint-send-string "*erlang*" (concat n ":main().\n"))
+    ))
+
+(defun my-erlang-kill ()
+  (interactive)
+  (my-kill-buffers "*erlang*"))
+
+(defun my-erlang-hook ()
+  (my-local-set-key (kbd "<f5>")
+    'save-buffer 'my-erlang-compile)
+  (my-local-set-key (kbd "<f6>")
+    'my-erlang-run)
+  (my-local-set-key (kbd "<f7>")
+    'my-erlang-kill))
+
+(add-hook 'erlang-mode-hook 'my-erlang-hook)
+(autoload 'erlang-mode "erlang-start" "erlang..." t)
+(add-to-list 'auto-mode-alist '("\\.erl$" . erlang-mode))
+(setq inferior-erlang-machine-options '("-sname" "emacs"))
+
+;;===ocaml
+(defun my-ocaml-start ()
+  (interactive)
+  (save-excursion
+    (tuareg-run-process-if-needed "ocaml")))
+
+(defun my-ocaml-kill ()
+  (interactive)
+  (my-kill-buffers tuareg-interactive-buffer-name))
+
+(defun my-ocaml-hook ()
+  (auto-fill-mode 1)
+  (my-local-set-key (kbd "<f5>")
+    'save-buffer 'my-ocaml-start 'tuareg-eval-buffer)
+  (my-local-set-key (kbd "<f6>")
+    'save-buffer 'my-ocaml-start 'my-region-line 'tuareg-eval-region)
+  (my-local-set-key (kbd "<f7>")
+    'tuareg-kill-ocaml 'my-ocaml-kill))
+
+(add-hook 'tuareg-mode-hook 'my-ocaml-hook)
+(setq tuareg-interactive-scroll-to-bottom-on-output t)
+(setq tuareg-interactive-echo-phrase nil)
+(setq tuareg-lazy-= t)
+(setq tuareg-lazy-paren t)
+(setq tuareg-in-indent 0)
+(autoload 'tuareg-mode "tuareg" "Major mode for editing Caml code" t)
+(autoload 'camldebug "camldebug" "Run the Caml debugger" t)
+(add-to-list 'auto-mode-alist '("\\.ml[ily]?$" . tuareg-mode))
+
+;;===ruby
+(defun my-ruby-start ()
+  (interactive)
+  (let ((c (current-buffer)))
+    (run-ruby ruby-program-name)
+    (unless (eq (current-buffer) c)
+      (other-window 1))))
+
+(defun my-ruby-send-buffer ()
+  (interactive)
+  (ruby-send-region (point-min) (point-max)))
+
+(defun my-ruby-kill ()
+  (interactive)
+  (my-kill-buffers "*ruby*"))
+
+(defun my-ruby-hook ()
+  (my-local-set-key (kbd "<f5>")
+    'save-buffer 'my-ruby-start 'my-ruby-send-buffer)
+  (my-local-set-key (kbd "<f6>")
+    'save-buffer 'my-ruby-start 'my-region-line 'ruby-send-region)
+  (my-local-set-key (kbd "<f7>")
+    'my-ruby-kill))
+
+(add-hook 'ruby-mode-hook 'my-ruby-hook)
+(autoload 'ruby-mode "inf-ruby" "ruby..." t)
+(add-to-list 'auto-mode-alist '("\\.rb$" . ruby-mode))
+
+;;===cobol
+
+(defun my-cobol-send-buffer ()
+  (interactive)
+
+  )
+
+(defun my-cobol-kill ()
+  (interactive)
+  (my-kill-buffers "*cobol*"))
+
+(defun my-cobol-hook ()
+  (my-local-set-key (kbd "<f5>")
+    'save-buffer 'my-cobol-send-buffer)
+  (my-local-set-key (kbd "<f7>")
+    'my-cobol-kill))
+
+(add-hook 'cobol-mode-hook 'my-cobol-hook)
+(autoload 'cobol-mode "cobol-mode" "Major mode for Tandem COBOL files." t nil)
+(add-to-list 'auto-mode-alist '("\\.cbl$" . cobol-mode))
+(add-to-list 'auto-mode-alist '("\\.cob$" . cobol-mode))
+
+;;===php
+(autoload 'php-mode "php-mode" "Major mode for editing php code." t)
+(add-to-list 'auto-mode-alist '("\\.php$" . php-mode))
+(add-to-list 'auto-mode-alist '("\\.inc$" . php-mode))
+
+;;===ragel
+;;(defun my-ragel-hook ()
+;;  (my-local-set-key (kbd "<f5>") 'save-buffer 'my-cpp-compile)
+;;  (my-local-set-key (kbd "<f6>") 'save-buffer 'my-cpp-run)
+;;  (my-local-set-key (kbd "<f7>") 'my-cpp-kill)
+;;  (my-local-set-key (kbd "<f8>") 'my-cpp-clean))
+
+;;(autoload 'ragel-mode "ragel-mode" "ragel..." t)
+;;(add-hook 'ragel-mode-hook 'my-ragel-hook)
+;;(add-to-list 'auto-mode-alist '("\\.rl$" . ragel-mode))
+
+;;===80 column line
+(autoload 'fci-mode "fill-column-indicator" "fci..." t)
+(setq fci-rule-column 80)
+(setq fci-rule-color "grey")
+(setq fci-always-use-textual-rule t)
+(add-to-list 'minor-mode-alist '(fci-mode ""))
+
+;;===
+(define-minor-mode my-extra "desc" nil nil nil )
+(provide 'my-extra)
