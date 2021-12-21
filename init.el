c2701b27a3ffd055a957e3cf1eb8f213c7d9acd3
andrewhills <andrewhills@users.noreply.github.com>
2021-12-21 23:35:01 +1100

    Update init.el

--- init.el
+++ init.el
@@ -545,6 +545,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ad-redefinition-action (quote accept))
  '(c-basic-offset 4)
  '(comint-scroll-to-bottom-on-input t)
  '(compilation-always-kill t)
